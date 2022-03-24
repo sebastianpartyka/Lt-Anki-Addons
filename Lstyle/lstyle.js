44eeb7feb68f8a9f810dca2dcb120902e34da7dd
Eltaurus-Lt <93875472+Eltaurus-Lt@users.noreply.github.com>
2022-03-24 04:05:20 +0300

    Update lstyle.js

--- Lstyle/lstyle.js
+++ Lstyle/lstyle.js
@@ -4,7 +4,7 @@ var truncname,cleanname;
 
 for (let n=0; n<decksN; n++) {
 cleanname=decks[n].innerHTML.replace(/^\s*<font style=(""|"color:false;")>\s*/g, "").replace(/\s*<\/font>\s*$/g, ""); //clears "Enhance main window" font tags
-truncname=cleanname.replace(/^([0-9]\.)+ /g, ""); //removes the ordinal number
+truncname=cleanname.replace(/^([0-9]+\.)+ /g, ""); //removes the ordinal number
 if (truncname!=cleanname) //apply styling to numbered decks only
 {
 decks[n].innerHTML="<div style=\"text-align: center\"><img src=\"icons\\"+truncname+".png\" style=\"vertical-align:middle; display:inline-block; margin-right: 30px\" onerror=\"this.style.display='none'\"><span><b><font size=+2>"+truncname+"</font></b></span></div><br>"}
